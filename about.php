5e65eff4dbf601dddc2bfff0c8bab0118f32215c
Geoffroy Carette <zou.zoudu71@hotmail.fr>
2018-09-29 13:26:55 +0200

    Ajout de la page about.php

---
+++ about.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+        crossorigin="anonymous">
+    <title>Mustache Enthusiast</title>
+</head>
+
+<body>
+    <?php include("header.inc.php"); ?>
+    <section>
+        <div class="container_about">
+            <div class="content_about">
+                <h1>MUSTACCHIO STARTED</h1>
+                <img src="media/photographer.jpg" alt="">
+                <div class="about_para">
+                    <p class="font">
+                        <h2>We Have Free Templates for Everyone</h2>
+                        Our website templates are created with inspiration, checked for quality and originality and
+                        meticulously sliced and coded. What’s more, they’re absolutely free! You can do a lot with
+                        them.
+                        You can modify them. You can use them to design websites for clients, so long as you agree with
+                        the
+                        Terms of Use. You can even remove all our links if you want to.
+                    </p>
+                </div>
+                <div class="about_para">
+                    <p>
+                        <h2>We Have More Templates for You</h2>
+                        Looking for more templates? Just browse through all our Free Website Templates and find what
+                        you’re
+                        looking for. But if you don’t find any website template you can use, you can try our Free Web
+                        Design service and tell us all about it. Maybe you’re looking for something different,
+                        something
+                        special. And we love the challenge of doing something different and something special.
+                    </p>
+                </div>
+                <div class="about_para">
+                    <p>
+                        <h2>Be Part of Our Community</h2>
+                        If you’re experiencing issues and concerns about this website template, join the discussion on
+                        on
+                        our forum and meet other people in the community who share the same interests with you.
+                    </p>
+                </div>
+                <div class="about_para">
+                    <p>
+                        <h2>Template details</h2>
+                        Design version 2. Code version 1. Website Template details, discussion and updates for this
+                        Mustache Enthusiast Website Template. Website Template design by Free Website Templates. Please
+                        feel free to remove some or all the text and links of this page and replace it with your own
+                        About
+                        content.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include("footer.inc.php"); ?>
+
+</body>
+
+</html>
--- header.inc.php
+++ header.inc.php
@@ -4,8 +4,8 @@
                 <a href="index.html"><img class="logo" src="media/logo.jpg" alt="logo moustache" width="340" height="93"></a>
                 <nav>
                     <ul>
-                        <li><a href="#" class="nav_style">HOME</a></li>
-                        <li><a href="#" class="nav_style">ABOUT</a></li>
+                        <li><a href="index.html" class="nav_style">HOME</a></li>
+                        <li><a href="about.php" class="nav_style">ABOUT</a></li>
                         <li><a href="#" class="nav_style">GALLERY</a></li>
                         <li><a href="#" class="nav_style">BLOG</a></li>
                         <li><a href="#" class="nav_style">CONTACT</a></li>
