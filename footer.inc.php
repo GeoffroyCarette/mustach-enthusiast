0ec9a8dfc4d0263b28b62490e494f4e6893a5b23
Geoffroy Carette <zou.zoudu71@hotmail.fr>
2018-09-29 03:49:15 +0200

    Conversion .php

---
+++ footer.inc.php
@@ -0,0 +1,15 @@
+<footer class="background_color_footer">
+        <div class="container_footer">
+            <div class="content_footer">
+                <div>
+                    <p>© 2018 by Geoffroy. All rights reserved.</p>
+                </div>
+                <div class="img_all">
+                    <a href="#" class="twitter_logo"><i class="fab fa-twitter-square"></i></a>
+                    <a href="#" class="facebook_logo"><i class="fab fa-facebook-square"></i></a>
+                    <a href="#" class="gplus_logo"><i class="fab fa-google-plus-square"></i></a>
+                    <a href="#" class="pinterest_logo"><i class="fab fa-pinterest-square"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ header.inc.php
@@ -0,0 +1,16 @@
+<header>
+        <div class="container_header">
+            <div class="container_nav">
+                <a href="index.html"><img class="logo" src="media/logo.jpg" alt="logo moustache" width="340" height="93"></a>
+                <nav>
+                    <ul>
+                        <li><a href="#" class="nav_style">HOME</a></li>
+                        <li><a href="#" class="nav_style">ABOUT</a></li>
+                        <li><a href="#" class="nav_style">GALLERY</a></li>
+                        <li><a href="#" class="nav_style">BLOG</a></li>
+                        <li><a href="#" class="nav_style">CONTACT</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
